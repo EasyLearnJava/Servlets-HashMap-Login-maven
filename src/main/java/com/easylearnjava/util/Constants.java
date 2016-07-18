edc9a73bfb8103f4f0fc17380da2d6d08f1d62d5
Easy Learn Java <easylearnjava4u@gmail.com>
2016-07-18 09:36:28 -0500

    Test changes

--- src/main/java/com/easylearnjava/util/Constants.java
+++ src/main/java/com/easylearnjava/util/Constants.java
@@ -8,6 +8,5 @@ public class Constants {
 	public static final String LOGIN_SUCCESS_WELCOME_MESSAGE = "Successfully logged in to the application ... :)"; 
 	public static final String LOGIN_DATA_VALIDATION_ERROR = "username/password cannot be empty, <br> must contain atleast 5 characters each";
 	public static final String LOGIN_INVALID_CREDENTIALS = "Invalid login credentials";
-	
 
 }
